2e5095e93405f69607f80acf2cc8bd8a1f3ea321
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-29 15:15:05 -0300

    Add jokenpo rules

--- jokenpo.js
+++ jokenpo.js
@@ -57,9 +57,27 @@ function set_hands_click_listeners(){
 
   images.forEach(element => {
     element.addEventListener('click',(event)=>{
-      increase_score({score, who:'player'});
-      update_score_view()
+      play_jokenpo(element);
     });
   });
 
+}
+
+function play_jokenpo(element){
+  let player_move = element.id.split('_')[1]
+  let computer_move = 'rock'
+
+  //jokenpo rules
+  if((player_move == 'rock' && computer_move == 'scissor') || 
+    (player_move == 'scissor' && computer_move == 'paper') ||
+    (player_move == 'paper' && computer_move == 'rock')){
+    increase_score({score, who:'player'});
+    update_score_view();
+  }else if(player_move == computer_move){
+    //then do nothing...
+  }
+  else{
+    increase_score({score, who:'computer'});
+    update_score_view()
+  }
 }
