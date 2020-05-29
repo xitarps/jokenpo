db9bdfc44f7aa6c7d75e5d681715e9e0f7fa9ed6
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-29 13:29:42 -0300

    Add mouseover and mouseleave listeners

---
+++ jokenpo.js
@@ -0,0 +1,23 @@
+window.onload = ()=>{
+  set_image_visibility_listenner()
+}
+
+function fetch_images(){
+  return document.querySelectorAll('img')
+}
+function set_image_visibility_listenner(){
+  let images = fetch_images()
+
+  images.forEach(element => {
+    element.style.opacity = 0.5
+  });
+
+  images.forEach(element => {
+    element.addEventListener('mouseover',(event)=>{
+      element.style.opacity = 1;
+    })
+    element.addEventListener('mouseleave',(event)=>{
+      element.style.opacity = 0.5;
+    })
+  });
+}
