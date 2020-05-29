fc102935cd242c165ec9c27be5a9df5e25b4cd60
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-29 15:59:07 -0300

    Add visual response of score and winning

--- jokenpo.js
+++ jokenpo.js
@@ -1,19 +1,23 @@
-let score= {player: 0 ,computer: 0}
+let score = {player: 0 ,computer: 0};
+let green = '#1eff00';
+let yellow ='#e9fa00';
+let blue = '#0015ce';
+let red = '#ff0000';
 
 window.onload = ()=>{
-  set_image_visibility_listenner()
+  set_image_visibility_listenner();
   set_hands_click_listeners();
 
 }
 
 function fetch_images(selector){
-  return document.querySelectorAll(selector)
+  return document.querySelectorAll(selector);
 }
 
 function set_image_visibility_listenner(){
-  let images = fetch_images('.choosable')
+  let images = fetch_images('.choosable');
 
-  set_images_to_not_selected()
+  set_images_to_not_selected('img');
   
   images.forEach(element => {
     element.addEventListener('mouseover',(event)=>{
@@ -25,31 +29,35 @@ function set_image_visibility_listenner(){
   });
 }
 
-function set_images_to_not_selected(){
-  let images = fetch_images('img')
+function set_images_to_not_selected(selector){
+  let images = fetch_images(selector);
   images.forEach(element => {
-    element.style.opacity = 0.5
+    element.style.opacity = 0.5;
   });
+}
+function set_images_to_selected(move){
+  let image = document.querySelector(`#computer_${move}_img`);
+  image.style.opacity = 1;
 }
 
 function reset_score(){
-  let score = document.querySelectorAll('.score')
+  let score = document.querySelectorAll('.score');
   score.forEach(element => {
-    element.innerHTML = 0
+    element.innerHTML = 0;
   });
 }
 
 function increase_score({score,who}){
   if(who == 'player')return score.player++;
-  score.computer++;
+  score.computer++;;
 }
 
 function update_score_view(){
   let player_score = document.querySelector('#player_score');
   let computer_score = document.querySelector('#computer_score');
 
-  player_score.innerHTML = score.player
-  computer_score.innerHTML = score.computer
+  player_score.innerHTML = score.player;
+  computer_score.innerHTML = score.computer;
 }
 
 function set_hands_click_listeners(){
@@ -63,12 +71,39 @@ function set_hands_click_listeners(){
 
 }
 
+function set_notice(message){
+  let notice = document.querySelector('#notice');
+  notice.innerHTML = message;
+}
+function set_notice_color(color){
+  let notice = document.querySelector('#notice');
+  notice.style.backgroundColor = color;
+}
+function set_winning_color(){
+  let player = document.querySelector("#player");
+  let computer = document.querySelector("#computer");
+
+  if(score.player > score.computer){
+    player.style.backgroundColor = green;
+    computer.style.backgroundColor = red;
+  }else if(score.player < score.computer){
+    player.style.backgroundColor = red;
+    computer.style.backgroundColor = green;
+  }else{
+    player.style.backgroundColor = blue;
+    computer.style.backgroundColor = blue;
+  }
+
+}
+
 function play_jokenpo(element){
   let moves = ['rock','paper','scissor']
   let player_move = element.id.split('_')[1]
   let randon_zero_one_or_two = Math.floor(Math.random() * 3);
   let computer_move = moves[randon_zero_one_or_two]
-  console.log(computer_move)
+  
+  set_images_to_not_selected('.computer_hand_img');
+  set_images_to_selected(computer_move);
 
   //jokenpo rules
   if((player_move == 'rock' && computer_move == 'scissor') || 
@@ -76,11 +111,19 @@ function play_jokenpo(element){
     (player_move == 'paper' && computer_move == 'rock')){
     increase_score({score, who:'player'});
     update_score_view();
+    set_notice('Nice! you won!');
+    set_notice_color(green);
+
   }else if(player_move == computer_move){
-    //then do nothing...
+    set_notice('What?!  A tie ?!');
+    set_notice_color(yellow);
+
   }
   else{
     increase_score({score, who:'computer'});
-    update_score_view()
+    update_score_view();
+    set_notice('Ops... maybe next time, huh?');
+    set_notice_color(red);
   }
+  set_winning_color();
 }
