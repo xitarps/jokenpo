3b0ed1430bfe6dd4159bf7c9aa10e40878430624
Rodrigo Pimentel satyro <xitarps@gmail.com>
2020-05-29 14:50:10 -0300

    Fix cpu hands on mouse actions disable

--- jokenpo.js
+++ jokenpo.js
@@ -2,15 +2,14 @@ window.onload = ()=>{
   set_image_visibility_listenner()
 }
 
-function fetch_images(){
-  return document.querySelectorAll('img')
+function fetch_images(selector){
+  return document.querySelectorAll(selector)
 }
-function set_image_visibility_listenner(){
-  let images = fetch_images()
 
-  images.forEach(element => {
-    element.style.opacity = 0.5
-  });
+function set_image_visibility_listenner(){
+  let images = fetch_images('.choosable')
+
+  set_images_to_not_selected()
   
   images.forEach(element => {
     element.addEventListener('mouseover',(event)=>{
@@ -19,5 +18,12 @@ function set_image_visibility_listenner(){
     element.addEventListener('mouseleave',(event)=>{
       element.style.opacity = 0.5;
     })
+  });
+}
+
+function set_images_to_not_selected(){
+  let images = fetch_images('img')
+  images.forEach(element => {
+    element.style.opacity = 0.5
   });
 }
